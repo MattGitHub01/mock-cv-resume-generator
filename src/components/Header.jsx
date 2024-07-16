079f975c8ec5a3ee0fd3b2c37b1cbe679bac75d5
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-07-16 11:25:49 -0600

    Add Header component, update CSS & App.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <h1>CV Helper</h1>
+            <p>Webapp tool to help you create your own CV in minutes!</p>
+        </div>
+    )
+}
+
+export default Header
